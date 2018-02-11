634647bf30419a534b175178a386cacadf212a2a
Lipegno <filp3q@gmail.com>
2018-02-11 17:38:34 +0000

    New icons for tabs

--- wear/src/main/java/com/example/filipe/socketcontroller/WattappTabs.java
+++ wear/src/main/java/com/example/filipe/socketcontroller/WattappTabs.java
@@ -4,10 +4,10 @@ public enum WattappTabs
 {
     STARTSTOP(R.string.TITLE_STARTSTOP, android.R.drawable.ic_media_play,R.layout.tab_start_stop,R.menu.menu_start_stop),
     SCHEDULE(R.string.TITLE_SCHEDULE, android.R.drawable.ic_lock_idle_alarm,R.layout.tab_schedule, -1),
-    STATS1(R.string.TITLE_STATS1, android.R.drawable.stat_sys_data_bluetooth,R.layout.tab_stats1,-1),
-    STATS2(R.string.TITLE_STATS2, android.R.drawable.stat_sys_data_bluetooth,R.layout.tab_stats2,-1),
-    STATS3(R.string.TITLE_STATS3, android.R.drawable.stat_sys_data_bluetooth,R.layout.tab_stats3,-1),
-    LOG(R.string.TITLE_LOG, android.R.drawable.ic_menu_preferences,R.layout.tab_log,-1);
+    STATS1(R.string.TITLE_STATS1, android.R.drawable.ic_menu_sort_by_size,R.layout.tab_stats1,-1),
+    STATS2(R.string.TITLE_STATS2, android.R.drawable.ic_menu_sort_by_size,R.layout.tab_stats2,-1),
+    STATS3(R.string.TITLE_STATS3, android.R.drawable.ic_menu_sort_by_size,R.layout.tab_stats3,-1),
+    LOG(R.string.TITLE_LOG, android.R.drawable.ic_menu_info_details,R.layout.tab_log,-1);
 
     final int title;
     final int icon;
