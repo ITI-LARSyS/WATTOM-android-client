d41c3fc8b99d026ef971d99126b03460bac2bf52
Lipegno <filp3q@gmail.com>
2018-02-12 01:23:52 +0000

    Layout removed from enum (was unnecessary)

--- wear/src/main/java/com/example/filipe/socketcontroller/WattappTabConfig.java
+++ wear/src/main/java/com/example/filipe/socketcontroller/WattappTabConfig.java
@@ -5,53 +5,45 @@ public enum WattappTabConfig
     STARTSTOP(R.string.TITLE_STARTSTOP,
             android.R.drawable.ic_media_play,
             R.id.tab_start_stop,
-            R.layout.tab_start_stop,
             R.menu.menu_start_stop),
 
     SCHEDULE(R.string.TITLE_SCHEDULE,
             android.R.drawable.ic_lock_idle_alarm,
             R.id.tab_schedule,
-            R.layout.tab_schedule,
             -1),
 
     STATS1(R.string.TITLE_STATS1,
             android.R.drawable.ic_menu_sort_by_size,
             R.id.tab_stats1,
-            R.layout.tab_stats1,
             -1),
 
     STATS2(R.string.TITLE_STATS2,
             android.R.drawable.ic_menu_sort_by_size,
             R.id.tab_stats2,
-            R.layout.tab_stats2,
             -1),
 
     STATS3(R.string.TITLE_STATS3,
             android.R.drawable.ic_menu_sort_by_size,
             R.id.tab_stats3,
-            R.layout.tab_stats3,
             -1),
 
     LOG(R.string.TITLE_LOG,
             android.R.drawable.ic_menu_info_details,
             R.id.tab_log,
-            R.layout.tab_log,
             -1);
 
     final int title;
     final int icon;
     final int id;
-    final int layout;
     final int menu;
 
     public static final WattappTabConfig DEFAULT = STARTSTOP;
 
-    WattappTabConfig(final int title, final int icon, final int id, final int layout, final int menu)
+    WattappTabConfig(final int title, final int icon, final int id, final int menu)
     {
         this.title = title;
         this.icon = icon;
         this.id = id;
-        this.layout = layout;
         this.menu = menu;
     }
 }
