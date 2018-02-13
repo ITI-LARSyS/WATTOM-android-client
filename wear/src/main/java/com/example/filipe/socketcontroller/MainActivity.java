e9bbad636e42edbf972d876f915d5d13a7aceb92
Lipegno <filp3q@gmail.com>
2018-02-13 19:29:34 +0000

    stats2 and stats3 scratch menus dones

--- wear/src/main/java/com/example/filipe/socketcontroller/MainActivity.java
+++ wear/src/main/java/com/example/filipe/socketcontroller/MainActivity.java
@@ -39,8 +39,13 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
+import org.eazegraph.lib.charts.BarChart;
 import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
 import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -143,6 +148,8 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
     private PieChart mPieChart;
     private String [] ChartColor = new String[4];
     private TextView _consumo;
+    private BarChart mBarChart;
+    private ValueLineChart mCubicValueLineChart;
 
     /* ******************************************************************************** */
     /* ******************************************************************************** */
@@ -629,6 +636,47 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
         chooseEndTime   = (LinearLayout) globalView.findViewById(R.id.UltimoTempo);
         mPieChart       = (PieChart) globalView.findViewById(R.id.piechart);
         textSensorState = (TextView) globalView.findViewById(R.id.textSensorState);
+
+        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
+        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
+
+        mPieChart.startAnimation();
+
+        mBarChart = (BarChart) globalView.findViewById(R.id.barchart);
+
+        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
+        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
+        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
+        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
+
+        mBarChart.startAnimation();
+
+        mCubicValueLineChart = (ValueLineChart) globalView.findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+        series.addPoint(new ValueLinePoint("Mar", .4f));
+        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
 
         InitialTime.setIs24HourView(true);
         InitialTime.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
